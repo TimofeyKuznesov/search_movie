31e2e240458bda71920c6aec99649de13c7dba49
Timofei Kuznetsov <timofei_kuznetsov@epam.com>
2021-11-01 09:34:07 +0300

    feat: add layout for movie card

--- lib/app/core/widgets/movie-card/card.dart
+++ lib/app/core/widgets/movie-card/card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:search_movie/app/theme/index.dart';
 
 class MovieCard extends StatelessWidget {
   const MovieCard({Key? key}) : super(key: key);
@@ -6,20 +7,58 @@ class MovieCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        child: const Center(
-          child: Text('Card'),
-        ),
-        width: 100,
-        height: 100,
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.black,
-            width: 2,
+      child: Card(
+        child: Container(
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  Expanded(child: Image.asset('graphics/BitmapBohemian.png')),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            child: const Text(
+                              'Bohemian Rhapsody',
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ),
+                        const Text('2003'),
+                      ],
+                    ),
+                    flex: 0,
+                  )
+                ],
+              ),
+              Positioned(
+                top: 0,
+                right: 0,
+                child: SizedBox(
+                  height: 30,
+                  width: 30,
+                  child: FloatingActionButton(
+                    onPressed: () {},
+                    child: Icon(Icons.more_vert),
+                    backgroundColor: BrandingColors.black,
+                  ),
+                ),
+              ),
+            ],
           ),
-          borderRadius: BorderRadius.circular(12),
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.black,
+              width: 2,
+            ),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          margin: EdgeInsets.all(3),
         ),
-        margin: EdgeInsets.all(3),
+        color: BrandingColors.black,
       ),
     );
   }
