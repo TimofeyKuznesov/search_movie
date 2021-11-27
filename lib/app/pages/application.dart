276c506c612075999aa294c01aa0b2fe81d0c7bb
Timofei Kuznetsov <timofei_kuznetsov@epam.com>
2021-11-27 15:33:58 +0300

    feat: refactor routing

--- lib/app/pages/application.dart
+++ lib/app/pages/application.dart
@@ -1,38 +1,67 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:search_movie/app/core/widgets/dialog/exit_app.dart';
 import 'package:search_movie/app/pages/index.dart';
 import 'package:search_movie/app/services/localization/app.i18n.dart';
 import 'package:search_movie/app/theme/branding_colors.dart';
+import 'package:search_movie/app_route.dart';
 
 class Application extends StatelessWidget {
+  late final GlobalKey<NavigatorState> _navigatorKey =
+      GlobalKey<NavigatorState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Movie search'.i18n),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: const BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text('Header'.i18n),
-            ),
-            ListTile(
-              title: Text('menu1'.i18n),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
+    return WillPopScope(
+      onWillPop: () => exitApp(context),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Movie search'.i18n),
         ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text('Header'.i18n),
+              ),
+              ListTile(
+                title: Text('menu1'.i18n),
+                onTap: () {
+                  Navigator.pop(context);
+                  _navigatorKey.currentState?.pushNamed(AppRoute.search);
+                },
+              ),
+              ListTile(
+                title: Text('map'.i18n),
+                onTap: () {
+                  Navigator.pop(context);
+                  _navigatorKey.currentState?.pushNamed(AppRoute.map);
+                },
+              ),
+            ],
+          ),
+        ),
+        body: Navigator(
+          key: _navigatorKey,
+          initialRoute: AppRoute.home,
+          onGenerateRoute: (settings) {
+            if (settings.name == AppRoute.search) {
+              return MaterialPageRoute(builder: (context) => MainPage());
+            }
+            if (settings.name == AppRoute.map) {
+              return MaterialPageRoute(
+                  builder: (context) => MapViewContainer());
+            }
+
+            return MaterialPageRoute(builder: (context) => MainPage());
+          },
+        ),
+        backgroundColor: BrandingColors.gray,
       ),
-      body: const MainPage(),
-      //style
-      backgroundColor: BrandingColors.gray,
     );
   }
 }
